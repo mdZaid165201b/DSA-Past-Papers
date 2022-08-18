8414de0b69f44efaeedb6dbf77035958a7e03e0b
mdZaid165201b <45224@gcslahore.edu.pk>
2022-08-18 09:52:01 +0500

    Remove Duplicates From LinkedLIst 2015 Long

---
+++ Remove Duplicates From LinkedList/main.cpp
@@ -0,0 +1,263 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+    Node * next;
+    int data;
+public:
+    Node ()
+    {
+        data = -1;
+        next = NULL;
+    }
+    Node (int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node (Node * next)
+    {
+        this->next = next;
+        this->data = -1;
+    }
+    Node (int data, Node * next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+    //Getters and Setters
+    int getData ()
+    {
+        return data;
+    }
+    Node * getNext ()
+    {
+        return this->next;
+    }
+    void setData (int data)
+    {
+        this->data = data;
+    }
+    void setNext (Node * next)
+    {
+        this->next = next;
+    }
+};
+//  --------------LinkedList Class-------------------------
+class LinkedList
+{
+private:
+    Node *head;
+public:
+    LinkedList()
+    {
+        head = NULL;
+    }
+    LinkedList(int val)
+    {
+        insertAtLast(val);
+    }
+    bool isEmpty()
+    {
+        return (head == NULL) ? true : false;
+    }
+    int getLength()
+    {
+        int count = 0;
+
+
+        Node * temp = head;
+        while (temp != NULL)
+        {
+            temp = temp->getNext ();
+            count++;
+        }
+        return count;
+
+
+    }
+    void insertAtBegin(int val)
+    {
+        Node *newNode = new Node(val);
+        if(isEmpty())
+        {
+            head = newNode;
+        }
+        else
+        {
+            newNode->setNext(head);
+            head = newNode;
+
+        }
+
+    }
+    void insertAtLast(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if(isEmpty())
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while(temp->getNext() != NULL)
+            {
+                temp = temp->getNext();
+            }
+            temp->setNext(newNode);
+        }
+    }
+    bool removeFromBegin()
+    {
+        if(isEmpty())
+        {
+            return false;
+        }
+        else
+        {
+            if(getLength() == 1)
+            {
+                delete head;
+                head = NULL;
+            }
+            else
+            {
+                Node *temp = head;
+                head = temp->getNext();
+                delete temp;
+                return true;
+            }
+        }
+    }
+    bool removeFromLast()
+    {
+        if(isEmpty())
+        {
+            return false;
+        }
+        else
+        {
+            Node *temp,*prevNode;
+            temp = head;
+            while(temp->getNext() != NULL)
+            {
+                prevNode = temp;
+                temp = temp->getNext();
+            }
+            delete temp;
+            prevNode->setNext(NULL);
+            return true;
+        }
+    }
+    Node *getFirstElement()
+    {
+        return head;
+    }
+    Node *getLastElement()
+    {
+        Node *temp;
+        temp = head;
+        while(temp->getNext() != NULL)
+        {
+            temp = temp->getNext();
+        }
+        return temp;
+    }
+    void print()
+    {
+        Node *temp;
+        temp = head;
+        while(temp != NULL)
+        {
+            cout<<temp->getData()<<endl;
+            temp = temp->getNext();
+        }
+    }
+
+    Node *getElementFromIndex(int index)
+    {
+        if(isEmpty())
+        {
+            return NULL;
+        }
+        else
+        {
+            Node *temp = head;
+            if(index >=0 && index <=getLength())
+            {
+                for(int i = 0; i<=index; i++)
+                {
+                    if(i == index)
+                    {
+                        return temp;
+                    }
+                    else
+                    {
+                        temp= temp->getNext();
+                    }
+                }
+            }
+            else
+            {
+                return NULL;
+            }
+        }
+    }
+
+    // 2015 Long Question that is removeDuplicatesFromLinkedList()
+    void removeDuplicates()
+    {
+        Node *temp = head;
+        while(temp!=NULL)
+        {
+            Node *temp2 = temp->getNext();
+            Node *prev;
+            while(temp2!=NULL)
+            {
+                if(temp->getData() == temp2->getData())
+                {
+                    if(temp2->getNext() == NULL)
+                    {
+                        delete temp2;
+                        temp2 = NULL;
+                        prev->setNext(NULL);
+                        continue;
+                    }
+                    else
+                    {
+                        prev->setNext(temp2->getNext());
+                        Node *testNode = temp2;
+                        prev = temp2;
+                        delete testNode;
+                    }
+                }
+                prev = temp2;
+                temp2 = temp2->getNext();
+            }
+            temp = temp->getNext();
+        }
+    }
+};
+
+int main()
+{
+    LinkedList l1;
+    l1.insertAtLast(1);
+    l1.insertAtLast(4);
+    l1.insertAtLast(2);
+    l1.insertAtLast(4);
+    l1.insertAtLast(7);
+    l1.insertAtLast(1);
+    l1.print();
+    cout<<endl<<endl;
+    l1.removeDuplicates();
+    cout<<"After Runing removeDuplicates"<<endl;
+    l1.print();
+
+
+    return 0;
+}
