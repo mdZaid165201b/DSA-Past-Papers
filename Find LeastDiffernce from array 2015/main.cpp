b07d9dd1058f38b59e7054479fb5c1217288fd62
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-30 17:25:01 +0500

    Updated the sort function , and removed warning
    Updated the sort function , and removed warning

--- Find LeastDiffernce from array 2015/main.cpp
+++ Find LeastDiffernce from array 2015/main.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
-
 using namespace std;
 
-void sort(int arr[],int len){
-    for(int i = 0;i<len-1;i++){
-        for(int j = 0;j<len-1;j++){
-            if(arr[j]<arr[j+1]){
-                // no swap
-            }
-            else{
+void sort(int arr[] , int len){
+    for(int i = 0 ; i < (len - 1) ; i++){
+        for(int j = 0 ; j < (len - 1) ; j++){
+            if(arr[j] > arr[j+1]){ // replace > with < to make descending order
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int findLeastDifference(int arr[],int len){
+
+int findLeastDifference(int arr[] , int len){
     int least = arr[1]-arr[0];
-    for(int i = 0;i<len-1;i++){
-        int temp = arr[i+1]-arr[i];
-        if(temp<least){
+    for(int i = 0 ; i < len - 1 ; i++){
+        int temp = arr[i + 1] - arr[i];
+        if (temp < least){
             least = temp;
         }
     }
-    cout<<"least differnce =>"<<least<<endl;
+    return least;
 }
 
-void print(int arr[],int len){
-    for(int i = 0 ;i<len;i++){
-        cout<<arr[i]<<endl;
+void print(int arr[] , int len){
+    for(int i = 0; i < len; i++){
+        cout << arr[i] << " ";
     }
+    cout << endl;
 }
 
 int main()
@@ -40,8 +38,7 @@ int main()
     int len = sizeof(arr)/sizeof(arr[0]);
     sort(arr,len);
     print(arr,len);
-    cout<<endl<<endl;
-    findLeastDifference(arr,len);
+    cout << "Least Diff : " << findLeastDifference(arr,len) << endl;
 
     return 0;
 }
