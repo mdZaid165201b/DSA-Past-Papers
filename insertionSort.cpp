3272824853b534eba2f3e73c1b921e04b71755b2
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-09-07 00:46:24 +0500

    updated insertion loop using for loop

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -16,6 +16,22 @@ void insertionSort(int arr[], int length){
     }
 }
 
+// using for Loop
+void insertionSort(int arr[], int length){
+    for(int  i = 1; i < length; i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(;j >= 0; j--){
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }
+            else{ break; }
+        }
+        arr[j+1] = temp;
+    }
+}
+
+
 void print(int arr[], int length){
     for(int  i = 0; i < length; i++){
         cout<<arr[i]<<" ";
