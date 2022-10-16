80271ce8a757333bb88b16b8049d5e14b2c6e482
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-16 16:40:22 +0500

    Update 2018 Q3 ( a ) Long.cpp

--- 2018 Q3 ( a ) Long.cpp
+++ 2018 Q3 ( a ) Long.cpp
@@ -1,25 +1,30 @@
-
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
 
 int main()
 {
     char str[20];
+    str[0] = 'a';
+    str[1] = 'b';
+    str[2] = 'a';
+    str[3] = 'b';
+    str[4] = 'a';
+    str[5] = 'b';
     int count = 0;
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] != "\0"){ count++; }
+    for(int i = 0; i < strlen(str); i++){
+        if(str[i] != '\0'){ count++; }
     }
     bool check1, check2 = false;
-    for(int i = 0; i < (count/2) - 1; i++){
+    for(int i = 0; i < count / 2 - 1; i++){
         if(str[i] == str[i + 1]){ check1 = true; }
         else{
             check1 = false;
             break;
         }
     }
-    for(int i = (count / 2) - 1; i++){
+    for(int i = (count / 2); i <count - 1; i++){
         if(str[i] == str[i + 1]){
             check2 = true;
         }
