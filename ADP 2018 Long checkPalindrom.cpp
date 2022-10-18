3705a07aeefa9e6411849e23edd59258af636de2
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-18 21:17:13 +0500

    Update ADP 2018 Long checkPalindrom.cpp

--- ADP 2018 Long checkPalindrom.cpp
+++ ADP 2018 Long checkPalindrom.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 using namespace std;
 
-
+// Method #1
 bool checkPalidrom(string word){
     char chArr[word.length()];
     
@@ -12,6 +12,14 @@ bool checkPalidrom(string word){
     for(int i = 0; i < word.length(); i++){
         // cout<<"chArr :"<<chArr[i]<<" "<<"word :"<<word[word.length()-i];
         if(chArr[i] != word[word.length() - 1 - i]){ return false; }
+    }
+    return true;
+}
+
+// Method #2
+bool checkPalindrom2(string word){
+    for(int i = 0; i < word.length(); i++){
+        if(word[i] != word[word.length() - 1 - i]){ return false; }
     }
     return true;
 }
