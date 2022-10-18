26fa74411b399544cb1863d454a5a312165e76d6
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-18 20:31:04 +0500

    Create ADP 2018 Long checkPalindrom.cpp

---
+++ ADP 2018 Long checkPalindrom.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool checkPalidrom(string word){
+    char chArr[word.length()];
+    char chArr2[word.length()];
+    for(int i = 0;i < word.length(); i++){
+        chArr[i] = word[i];
+    }
+    for(int i = word.length(); i >= 0; i--){
+        chArr2[i] = word[i];
+    }
+    for(int i = 0; i < word.length(); i++){
+        if(chArr[i] != chArr2[word.length() - i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+int main()
+{
+    string word = "zaid";
+    checkPalidrom(word) ? cout<<"Palindrom" : cout<<"Not Palindrom";
+
+    return 0;
+}
