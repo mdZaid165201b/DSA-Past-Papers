2d178516d6ab49c30298c4a412763a6014c3f497
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-20 23:20:31 +0500

    Create findk() Long 2017 (b).cpp

---
+++ findk() Long 2017 (b).cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int findk(int arr[],int left, int right){
+    int middle = (left + right) / 2;
+    if(arr[left] > arr[left+1]){
+            cout<<endl<<"test :";
+            return left+1;
+    }
+    if(arr[left] < arr[middle]){
+         return findk(arr,middle,right);
+    }
+    else{
+        
+        return findk(arr,left,middle);
+    }
+}
+
+int main()
+{
+    int arr[14] = {9,13,16,18,19,23,28,-1,0,1,2,5,7,8};
+    cout<<findk(arr,0,15);
+    return 0;
+}
