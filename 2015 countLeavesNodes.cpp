fd2dfafe7691bad959dbce28969152e722262c20
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-10-22 23:27:12 +0500

    Update 2015 countLeavesNodes.cpp

--- 2015 countLeavesNodes.cpp
+++ 2015 countLeavesNodes.cpp
@@ -8,3 +8,13 @@ int countLeaves(Node *root){
         cout<<"temp :"<<temp<<endl;
         return 1+countLeaves(root->getRight());
  }
+
+// More Understandable code
+int countLeaves(Node *root){
+        if(root == NULL){ return 0; }
+        if(root->getLeft() == NULL && root->getRight() == NULL){ return 1; }
+        
+        else{
+            return countLeaves(root->getLeft()) + countLeaves(root->getRight());
+        }
+ }
